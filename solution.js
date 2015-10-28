ffcb9b1f57bb97f9acda9300e76c565fa3e429b6
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2015-10-28 15:46:23 -0700

    solution

---
+++ solution.js
@@ -0,0 +1,41 @@
+// challenge #1
+function multiply(a,b){
+	return a*b;
+}
+
+//challenge #2
+function isEven(num){
+	if (num%2===0){
+		return true;
+	}
+	else{
+			return false;
+		}
+}
+
+//chalenge #3
+
+function swap(arr, idx1, idx2){
+	var value1 = arr[idx1];
+	var value2 = arr[idx2];
+	arr[idx1] = value2;
+	arr[idx2] = value1;
+
+	return arr;
+
+}
+
+//challenge 4
+
+function getRand(min, max){
+	return (Math.random()*(max-min))+min;
+}
+
+// challenge 5
+function randArr(size){
+	var arr = [];
+	for(var i = 0; i<size; i++){
+		arr[i] = Math.random()*100;
+	}
+	return arr;
+}
